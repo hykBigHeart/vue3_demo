3005729659fe0ef44b6b52f7a561d6d352616559
huyikun <3069479901@qq.com>
2023-02-01 16:05:25 +0800

    watch监听属性

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -12,12 +12,24 @@
       全名：<span>{{data.fullName}}</span>
       <br>
       修改全名：<input v-model="data.fullName" type="text">
+      <hr>
+      <h2>求和：{{sum}}</h2>
+      <button @click="sum++">增加1</button>
+      <hr>
+      <h2>语音播报：{{msg}}</h2>
+      <button @click="msg+='!'">增加语音</button>
+      <h2>工作：{{data.work}}</h2>
+      <h2>年龄：{{data.age}}</h2>
+      <h2>工资：{{data.job.gongzi}}K</h2>
+      <button @click="data.work+='~'">工作变</button>
+      <button @click="data.age++">年龄增</button>
+      <button @click="data.job.gongzi++">工资加</button>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive } from "vue"; //  处理复杂数据类型专用
+import {ref, computed, reactive, watch } from "vue"; //  处理复杂数据类型专用
 
 export default {
   name: 'Demo',
@@ -31,26 +43,71 @@ export default {
     let data = reactive({
       firstName: '张',
       lastName: '三',
+      work: '前端',
+      age: 18,
+      job: {
+        gongzi: 15
+      }
     })
+    let sum = ref(0)
+    let msg = ref('你好')
     // 计算属性
     // data.fullName = computed(()=> {
     //   return data.firstName + '-' + data.lastName
     // })
-    data.fullName = computed({
-      get() {
-        return data.firstName + '-' + data.lastName
-      },
-      set(value) {
-        let nameArr = value.split('-')
-        data.firstName = nameArr[0]
-        data.lastName = nameArr[1]
-      }
-    })
+    // data.fullName = computed({
+    //   get() {
+    //     return data.firstName + '-' + data.lastName
+    //   },
+    //   set(value) {
+    //     let nameArr = value.split('-')
+    //     data.firstName = nameArr[0]
+    //     data.lastName = nameArr[1]
+    //   }
+    // })
+
+    // 情况一 监视ref定义的一个响应式数据
+    // watch(sum, (newVal, oldVal)=> {
+    //   console.log('sum', newVal, oldVal)
+    // }, {immediate: true}) //  immediate 立即执行
+
+    // 情况二 监视ref定义的多个响应式数据
+    // watch([sum, msg], (newVal, oldVal)=> {
+    //   console.log('sum或msg改变了', newVal, oldVal)
+    // }, {immediate: true}) 
+
+    // 情况三 监视reactive定义的一个响应式数据的全部属性
+    // 1. 此处无法获取正确的oldVal
+	  // 2. 已经强制开启了深度监听（deep配置了无效）
+    // watch(data, (newVal, oldVal)=> {  
+    //   console.log('监听data', newVal, oldVal)
+    // }, {deep: true}) //  deep 此处配置无效
+
+    // 情况四 监视reactive定义的一个响应式数据的某个属性
+    // watch(()=> data.work, (newVal, oldVal)=> {
+    //     console.log('监听data', newVal, oldVal)
+    // } )
+
+    // 情况五 监视reactive定义的一个响应式数据的某些属性
+    // watch(()=> [data.work, data.age], (newVal, oldVal)=> {
+    //     console.log('监听data', newVal, oldVal)
+    // } )
+    
+    // 特殊情况 监听reactive数据里的对象，需要开启deep
+    // 但是也无法正确获取oldVal
+    watch(()=> data.job, (newVal, oldVal)=> {
+      console.log('监听data', newVal, oldVal)
+    }, {deep: true} )
+
+
+
     function clickBtn() {
       context.emit('hello', '小明')
     }
     return {
       data,
+      sum,
+      msg,
       clickBtn
     }
   }
